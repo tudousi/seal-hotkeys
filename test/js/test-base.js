4d416d2850b65d0be0e9c5fa9b1132f7e30de112
tudousi <41871879@qq.com>
2016-03-19 18:13:28 +0800

    增加测试用例

--- test/js/test-base.js
+++ test/js/test-base.js
@@ -1,13 +1,26 @@
 describe("module base function", function() {
-    var elem = $('.superMan');
+    var res;
+    var elem = $('#superMan');
     var hotKey = new SealHotKeys({
-        //
+        el: "#superMan"
     });
     hotKey.add("ctrl + a", function(e){
-        //debugger;
+        res = 'aha';
     });
-    hotKey.remove('ctrl + a');
     it("shortcut format", function() {
         expect(SealHotKeys.keyFormat("shift + ctrl + a")).toBe("control_shift_a");
+    });
+    it("add shortcut", function() {
+        expect(hotKey._map.control_a).not.toBeUndefined();
+    })
+    it('event is triggered', function() {
+        var e = $.Event("keydown", {keyCode: 65, which: 65, ctrlKey: true, target: $('#superMan')[0]});
+        $(document).trigger(e);
+        expect(res).toBe("aha");
+    });
+
+    it('remove shortcut', function() {
+        hotKey.remove('ctrl + a');
+        expect(hotKey._map.control_a).toBeUndefined();
     });
 });
